50bb8cff158fc3756c357fed2af440e26bc0d985
Diya Chen <diya@cca.edu>
2019-11-13 15:09:09 -0800

    d
    d

---
+++ exhibition exercise/style2/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('big',pageResized)
+
+
+function pageResized(){
+  
+  let windowPercentOfScreen = (1 - (window.innerHeight / window.screen.height)) * 100
+  
+  let colorFromScreenHeight = 'hsl(0,0%,' + windowPercentOfScreen + '%)'
+  document.body.style.color = colorFromScreenHeight
+  
+}
+
+pageResized()
